bb5006b5b2a1180da954da9dc15c1cee699fb62e
Cauan Cabral <cauan@radig.com.br>
2011-09-02 14:25:11 -0400

    Versão inicial do plugin
    Incluído primeiro behavior para "proteção" do usuário, que efetua trim
    em qualquer string fornecido pelo mesmo antes de salvar/consultar modelos.

---
+++ models/behaviors/auto_trim.php
@@ -0,0 +1,140 @@
+<?php
+App::import('CORE', 'ConnectionManager');
+
+class AutoTrimBehavior extends ModelBehavior {
+	
+	private $_Model;
+	
+	/**
+	* Lista de formatos para os dados suportados pelo BD em uso.
+	* É recuperado automáticamente pela conexão com o banco.
+	*
+	* @var array
+	*/
+	private $_typesFormat;
+	
+	/**
+	* Inicializa os dados do behavior
+	*
+	* @see ModelBehavior::setup()
+	*/
+	public function setup(&$model, $config = array())
+	{
+		$this->_Model =& $model;
+		
+		$db =& ConnectionManager::getDataSource($this->_Model->useDbConfig);
+	
+		foreach($db->columns as $type => $info)
+		{
+			if(isset($info['format']))
+			{
+				$this->_typesFormat[$type] = $info['format'];
+			}
+		}
+	}
+	
+	/**
+	* Trim através do callback beforeValidate
+	*
+	* @see ModelBehavior::beforeValidate()
+	*/
+	public function beforeValidate(&$model)
+	{
+		$this->_Model =& $model;
+	
+		parent::beforeValidate($model);
+	
+		return $this->_autoTrim();
+	}
+	
+	/**
+	 * Trim através do callback beforeSave
+	 *
+	 * @see ModelBehavior::beforeSave()
+	 */
+	public function beforeSave(&$model)
+	{
+		$this->_Model =& $model;
+	
+		parent::beforeSave($model);
+	
+		return $this->_autoTrim();
+	}
+	
+	/**
+	 * Trim das informações no callback beforeFind
+	 *
+	 * @see ModelBehavior::beforeFind()
+	 */
+	public function beforeFind(&$model, $query)
+	{
+		$this->_Model =& $model;
+	
+		parent::beforeFind($mode, $query);
+	
+		$this->_autoTrim($query['conditions']);
+	
+		return $query;
+	}
+	
+	private function _autoTrim(&$query = null)
+	{
+		// verifica se há dados setados no modelo
+		if(isset($this->_Model->data) && !empty($this->_Model->data))
+		{
+			// varre os dados setados
+			foreach($this->_Model->data[$this->_Model->name] as $field => $value)
+			{
+				// caso o campo esteja vazio E não tenha um array como valor E o campo faz parte do schema
+				if(!empty($value) && !is_array($value) && isset($this->_Model->_schema[$field]))
+				{
+					switch($this->_Model->_schema[$field]['type'])
+					{
+						case 'string':
+						case 'text':
+							$this->_Model->data[$this->_Model->name][$field] = trim($this->_Model->data[$this->_Model->name][$field]);
+					}
+				}
+			}
+		}
+		
+		// caso tenha sido invocado em um Find (haja query de busca)
+		if(!empty($query) && is_array($query))
+		{
+			// varre os campos da condição
+			foreach($query as $field => &$value)
+			{
+				if(strtolower($field) === 'or' || strtolower($field) === 'and' || is_numeric($field))
+				{
+					$this->_autoTrim($value);
+					continue;
+				}
+			
+				// caso sejam campos com a notação Model.field
+				if(strpos($field, '.') !== false)
+				{
+					$ini = strpos($field, '.');
+					$len = strpos($field, ' ');
+						
+					$modelName = substr($field, 0, $ini - 1);
+						
+					if($len !== false)
+						$field = substr($field, $ini + 1, $len - $ini - 1);
+					else
+						$field = substr($field, $ini + 1);
+				}
+			
+				// caso o campo esteja vazio E não tenha um array como valor E o campo faz parte do schema
+				if(!empty($value) && isset($this->_Model->_schema[$field]))
+				{
+					switch($this->_Model->_schema[$field]['type'])
+					{
+						case 'string':
+						case 'text':
+							$value = trim($value);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ tests/fixtures/task_fixture.php
@@ -0,0 +1,21 @@
+<?php
+class TaskFixture extends CakeTestFixture {
+	public $name = 'Task';
+
+	public $fields = array(
+		'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false),
+		'term' => array('type'=>'date', 'null' => true),
+		'username' => array('type' => 'string', 'null' => false),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+
+	public $records = array(
+		array(
+			'id'  => 100,
+			'title' => 'The Mayan Prophecy',
+			'term'  => '2012-12-21',
+			'username'  => 'Good'
+		)
+	);
+}
