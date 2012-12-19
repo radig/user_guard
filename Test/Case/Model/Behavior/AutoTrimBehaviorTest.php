b431b5e1ab8849252e5335d4515e5f942c6cbcdd
Cauan Cabral <cauan@radig.com.br>
2012-12-19 16:48:04 -0300

    Correção dos testes com PHP 5.4 modo estrito

--- Test/Case/Model/Behavior/AutoTrimBehaviorTest.php
+++ Test/Case/Model/Behavior/AutoTrimBehaviorTest.php
@@ -21,23 +21,23 @@ class Task extends CakeTestModel
 
 
 class AutoTrimBehaviorTest extends CakeTestCase {
-	
+
 	public $name = 'AutoTrim';
-	
+
 	public $fixtures = array('plugin.user_guard.task');
-	
+
 	public $Task;
-	
-	public function startTest()
+
+	public function startTest($method)
 	{
-		$this->Task =& ClassRegistry::init('UserGuard.Task');
+		$this->Task = ClassRegistry::init('UserGuard.Task');
 	}
-	
-	public function endTest()
+
+	public function endTest($method)
 	{
 		unset($this->Task);
 	}
-	
+
 	/**
 	* Testa uma ação de busca
 	*/
@@ -46,7 +46,7 @@ class AutoTrimBehaviorTest extends CakeTestCase {
 		$result = $this->Task->find('all',
 			array('conditions' => array('username' => ' Good'))
 		);
-		
+
 		$expected = array(
 			array(
 				'Task' => array(
@@ -57,27 +57,27 @@ class AutoTrimBehaviorTest extends CakeTestCase {
 				)
 			)
 		);
-		
+
 		$this->assertEqual($result, $expected);
-		
+
 		$result = $this->Task->find('all',
 			array('conditions' => array('Task.username' => ' Good '))
 		);
-		
+
 		$this->assertEqual($result, $expected);
-		
+
 		$result = $this->Task->find('all',
 			array('conditions' => array('or' => array('Task.username' => ' Good  ', 'Task.id' => 100)))
 		);
-		
+
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	/*public function testValidateAction()
 	{
-		
+
 	}*/
-	
+
 	public function testSaveAction()
 	{
 		$this->Task->save(array(
@@ -86,11 +86,11 @@ class AutoTrimBehaviorTest extends CakeTestCase {
 			'term' => '2014-04-20',
 			'username' => ' Brazil '
 		));
-		
+
 		$result = $this->Task->find('first',
 			array('conditions' => array('username' => 'Brazil'))
 		);
-		
+
 		$expected = array(
 			'Task' => array(
 				'id'  => 1,
@@ -99,7 +99,7 @@ class AutoTrimBehaviorTest extends CakeTestCase {
 				'username'  => 'Brazil'
 			)
 		);
-	
+
 		$this->assertEqual($result, $expected);
 	}
 }
